15ef3bcadf6cfd5e4d47a5bb6513265fb74cc051
ainzle <87570181+ainzle@users.noreply.github.com>
2023-03-19 14:17:47 +0800

    Service

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators/map';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+  private _albumUrl  = '../assets/album.json';
+
+  getAlbum(id: number): Observable<any> {
+    return this._http.get(this._albumUrl).pipe
+    (map(response => response.json()))
+  };
+}
